d6f9970acec2c28779535b1d95b880391a5de160
Eric Snow <ericsnowcurrently@gmail.com>
2015-09-04 11:18:21 -0600

    Enmerate the known-to-Go OS names.

---
+++ version/os/os.go
@@ -0,0 +1,20 @@
+// Copyright 2015 Eric Snow
+// Licensed under the New BSD License, see LICENSE file for details.
+
+package os
+
+// These are the names of the operating systems recognized by Go.
+const (
+	Unknown = ""
+
+	Darwin    = "darwin"
+	Dragonfly = "dragonfly"
+	FreeBSD   = "freebsd"
+	Linux     = "linux"
+	Nacl      = "nacl"
+	NetBSD    = "netbsd"
+	OpenBSD   = "openbsd"
+	Solaris   = "solaris"
+
+	Windows = "windows"
+)
